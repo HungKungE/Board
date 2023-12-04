3756ad82412dff325e3a2f3dfc43ea05ae9c2e83
HungKungE <Jonsun59@gmail.com>
2023-12-04 15:03:30 +0900

    [server] login 기능에 비밀번호 비교 로직 추가

--- server/src/auth/api/logIn.ts
+++ server/src/auth/api/logIn.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { getUserInfo } from "../../db/context/userContext";
 import { mapToUserInfo } from "../../db/entity/user";
+import { serializePassword } from "../../utils/serialize";
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ const checkPassword = (userInfo: any) => {};
 router.post("/", async (req: Request, res: Response) => {
   const { nickname, password } = req.body;
 
-  // 닉네임 -> 사용자 정보 불러오기
+  // 닉네임 -> 사용자 정보 불러오기 ======================================
   const userInfo = await getUserInfo(nickname);
 
   if (userInfo.user_id == -1) {
@@ -19,7 +20,18 @@ router.post("/", async (req: Request, res: Response) => {
     return;
   }
 
-  /* 비밀번호 비교 로직 작성 */
+  // 비밀번호 비교 ======================================================
+  const serializedPassword: string = await serializePassword(
+    nickname,
+    password
+  );
+
+  if (userInfo.password != serializedPassword) {
+    res
+      .status(401)
+      .json({ success: false, error: "비밀번호가 일치하지 않습니다!" });
+    return;
+  }
 
   res.status(200).json({
     success: true,
