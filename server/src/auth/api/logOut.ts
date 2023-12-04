456bfb5d4265ff34db73604ce7b5dade159bd7b9
HungKungE <Jonsun59@gmail.com>
2023-12-04 22:09:31 +0900

    [server] 로그 아웃 시, redis session에서 삭제

--- server/src/auth/api/logOut.ts
+++ server/src/auth/api/logOut.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express";
-import { userInfo } from "os";
 
 const router = express.Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  // 세션에서 정보 제거
-  req.session.destroy;
+  delete req.session.userInfo;
 
   res.status(200).json({
     success: true,
